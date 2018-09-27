c408c22e5543bcd4e50418c6325637ba32de8908
Stefan Matting <stefan.matting@gmail.com>
2018-09-27 20:30:33 +0200

    update configuration.nix

--- thinkpad/etc/nixos/configuration.nix
+++ thinkpad/etc/nixos/configuration.nix
@@ -9,19 +9,48 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./logstash-logging.nix
+      ./filebeat.nix
     ];
+  
+  filebeat.logstash.host = "localhost";
 
   nixpkgs.config.allowUnfree = true;
 
+  # fileSystems."/home/stefan/askby_files" = {
+  #     device = "//files.askby.net/askby_files";
+  #     fsType = "cifs";
+  #     options = let
+  #       # this line prevents hanging on network split
+  #       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  #     in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=stefan,gid=users,_netdev"];
+  # };
+
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.efiSupport = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    grub = {
+      efiSupport = true;
+      extraEntriesBeforeNixOS = ''
+        menuentry "Win10" {
+          insmod part_gpt
+          insmod fat
+          insmod search_fs_uuid
+          insmod chain
+          search --fs-uuid --no-floppy --set=root AE8B-C046
+          chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+        }
+      '';
+    };
+  };
 
 
   # networking.hostName = "nixos"; # Define your hostname.
   networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
+  # networking.nameservers = ["213.239.242.238"];
+  networking.networkmanager.appendNameservers = ["8.8.8.8"];
 
   # Select internationalisation properties.
   # i18n = {
@@ -50,6 +79,42 @@
 
   # List services that you want to enable:
 
+   nix = {
+       # buildMachines = [{
+       #   hostName = "localhost";
+       #   systems = [ "x86_64-linux" ];
+       # }];
+       trustedUsers = [ "root" "stefan" ];
+
+       binaryCaches = [
+           http://18.185.36.89:8080
+           https://cache.nixos.org/
+          "https://hie-nix.cachix.org"
+       ];
+       binaryCachePublicKeys = [
+           "askby-1:fXvc/TFyDlDENEb5U35P+UgLhfpHn6mCYemxtzxtS+k="
+           "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+           "hie-nix.cachix.org-1:EjBSHzF6VmDnzqlldGXbi0RM3HdjfTU3yDRi9Pd0jTY="
+       ];
+
+       nixPath = [
+           "nixpkgs-overlays=/home/stefan/repos/apkgs/overlays"
+           "apkgs=/home/stefan/repos/apkgs"
+           "ssh-config-file=/nix/store/4vxmfvdj9fl5adw48y5sq5hnxqg25by2-config"
+           "nixpkgs=/home/stefan/repos/askby_nixpkgs"
+           "nixos-config=/etc/nixos/configuration.nix"
+       ];
+   };
+
+
+  # services.hydra = {
+  #   enable = false;
+  #   hydraURL = "http://localhost";
+  #   notificationSender = "stefan@localhost";
+  #   buildMachinesFiles = [];
+  #   useSubstitutes = true;
+  # };
+
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
@@ -63,12 +128,13 @@
   services.printing.enable = true;
 
   services.upower.enable = true;
+  services.redis.enable = true;
 
   services.xserver = {
     # Enable the X11 windowing system.
     enable = true;
     layout = "us";
-    # services.xserver.xkbOptions = "eurosign:e";
+    xkbOptions = "compose:caps";
   
     # Enable touchpad support.
     libinput.enable = true;
@@ -96,10 +162,87 @@
     isNormalUser = true;
     home = "/home/stefan";
     description = "Stefan";
-    extraGroups = ["wheel" "networkmanager"];
+    extraGroups = ["wheel" "networkmanager" "input"];
     shell = pkgs.zsh;
     uid = 1000;
   };
+
+  programs.slock.enable = true;
+
+  networking.firewall.allowedTCPPorts = [5672 15672];
+
+  services.rabbitmq = {
+      enable = true;
+      listenAddress = "0.0.0.0";
+      port = 5672;
+      plugins = ["rabbitmq_management"];
+      cookie = "ri4zYzjkyU98WMLQ";
+  };
+
+  # NOTE: run this once after creation
+  systemd.services.rabbitmq-setup = {
+      serviceConfig.Type = "oneshot";
+      enable = true;
+      script = ''
+          cat /var/lib/rabbitmq/.erlang.cookie > /root/.erlang.cookie
+          chmod 0600 /root/.erlang.cookie
+          export HOME=/root
+          export PATH=/run/current-system/sw/bin/:$PATH
+          rabbitmqctl add_user stefan foobar
+          rabbitmqctl set_user_tags stefan administrator
+          rabbitmqctl set_permissions -p / stefan ".*" ".*" ".*"
+      '';
+  };
+
+  systemd.services.hoogle = {
+    enable = true;
+    script = ''
+      /home/stefan/.nix-profile/bin/hoogle server -p 7777 --local
+    '';
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  services.prometheus = {
+    enable = true;
+    scrapeConfigs = [
+      {
+      job_name = "prometheus";
+      static_configs = [{targets = ["localhost:9090"];}];
+    }
+      {
+      job_name = "bibot";
+      scrape_interval = "1s";
+      static_configs = [{targets = ["localhost:8280"];}];
+    }
+      {
+      job_name = "wtproxy";
+      scrape_interval = "1s";
+      static_configs = [{targets = ["localhost:8281"];}];
+    }
+    ];
+  };
+
+  services.grafana = {
+    enable = true;
+    port = 3301;
+    # NOTE: this doesn't work afterwards
+    # security =  {
+    #   adminUser = "askby";
+    #   adminPassword = "smtvftlw";
+    # };
+  };
+
+  # services.autofs = {
+  #   enable = true;
+  #   autoMaster = 
+  #   (let
+  #     mapConf = pkgs.writeText "auto" ''
+  #      askby_files  -fstype=cifs,credentials=/etc/nixos/smb-secrets,uid=stefan,gid=users //files.askby.net/askby_files
+  #     '';
+  #    in ''
+  #     /auto file:${mapConf}
+  #    '');
+  # };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
@@ -107,4 +250,69 @@
   # should.
   system.stateVersion = "17.09"; # Did you read the comment?
 
+  # systemd.services.backup-askby-files = {
+  #     serviceConfig.Type = "oneshot";
+  #     script = ''
+  #     export AWS_SHARED_CREDENTIALS_FILE="/home/stefan/.aws/s3-credentials";
+  #     export PATH=${pkgs.awscli}/bin:$PATH
+  #     aws s3 sync /home/stefan/askby/ s3://askby-backup/askby_files
+  #     '';
+  # };
+
+  # systemd.timers.backup-askby-files = {
+  #     partOf = [ "backup-askby-files.service" ];
+  #     wantedBy = [ "timers.target" ];
+  #     timerConfig = {
+  #         OnCalendar = "hourly";
+  #     };
+  # };
+
+  services.openvpn.servers = {
+      aws = {
+          autoStart = true;
+
+          config = ''
+          remote 18.195.23.67 1194
+          client
+          dev tun
+          proto udp
+          resolv-retry infinite
+          nobind
+          persist-key
+          persist-tun
+          remote-cert-tls server
+          remote-cert-ku a0
+
+          ;cipher AES-128-CBC
+          ;auth SHA256
+          comp-lzo
+          verb 1
+
+          pkcs12 /home/stefan/.openvpn/stefan.p12
+
+          <tls-auth>
+          -----BEGIN OpenVPN Static key V1-----
+          5be0a3241fce9547bff5223cdec19868
+          17d58ed2a39885b236596355331f596a
+          8342ea20ba69fc8d2ac5de06314f04a5
+          ce35d88252e3e447c4115a7b3f219208
+          08da0ac941e3f0fcb2062ab60dd5f102
+          322943c95864e3e149aaf33cb91ce1ee
+          ee2835dd47a4ce0a12e175255da63dc4
+          712fed6e99d256e52ecbc5b742a328fc
+          3c0f79ded5a047df02b32e4e7ceb2c3d
+          6ccdbc3ca0ffa474f60ef84f7aae276d
+          5985b998e501d75afdb246b01f2dd99c
+          7c20d96d9238ed0dacbdfd367421a045
+          d6a2a1dde2cf1d910baa7f4f94b637ba
+          7540914ec2c443fbbeba747ecba6a667
+          5642e07fcd4477b2ac37f2e4e384e4a0
+          7209002c4d0fc686437bbd5e7b3fa63e
+          -----END OpenVPN Static key V1-----
+          </tls-auth>
+          '';
+      };
+  };
+
+  services.postgresql.enable = true;
 }
