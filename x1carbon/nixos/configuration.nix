97a31dc639abf43ab1e32b0c2292e652712c05c3
Stefan Matting <stefan.matting@gmail.com>
2022-07-10 23:48:02 +0200

    x1: install flakes

--- x1carbon/nixos/configuration.nix
+++ x1carbon/nixos/configuration.nix
@@ -21,6 +21,10 @@
     binaryCachePublicKeys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
   };
 
 
