657a0088346919c275db98f6822f275cd86de4d4
Stefan Matting <stefan@wire.com>
2023-08-31 12:05:25 +0200

    Add t14s_gen3

---
+++ t14s_gen3/nixos/cachix/smatting2.nix
@@ -0,0 +1,11 @@
+
+{
+  nix.settings = {
+    substituters = [
+      "https://smatting2.cachix.org"
+    ];
+    trusted-public-keys = [
+      "smatting2.cachix.org-1:syqlnq5yNXfDXftgZyukRT8pd2uneOYppJyvUYvXHus="
+    ];
+  };
+}
