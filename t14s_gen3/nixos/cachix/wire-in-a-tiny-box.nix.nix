80aec0f57650331ef53e9657823b08a4f88ce3b0
Stefan Matting <stefan@wire.com>
2024-09-02 11:36:45 +0200

    add wire-in-a-tiny-box cache

---
+++ t14s_gen3/nixos/cachix/wire-in-a-tiny-box.nix.nix
@@ -0,0 +1,10 @@
+{
+  nix.settings = {
+    substituters = [
+      "https://wire-in-a-tiny-box.cachix.org"
+    ];
+    trusted-public-keys = [
+      "wire-in-a-tiny-box.cachix.org-1:7kM+caFSvmhdQoZfF9H8/Oyo2PTRYvyFhR7exjQCAwU="
+    ];
+  };
+}
